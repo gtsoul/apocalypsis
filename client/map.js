e8126104b4785c94829935b2deb993510ba214b0
Arnaud Demaiziere <arnaud.demaiziere@gmail.com>
2013-04-24 14:03:01 +0200

    réécriture propre

--- client/map.js
+++ client/map.js
@@ -16,8 +16,6 @@ var Map = function(mapDiv) {
   this.zoom = 1;
 	this.mapDiv = mapDiv;
   this.mapRoot = jQuery(this.mapDiv).children('*:first');
-  this.offsetLeft = 0;
-  this.offsetTop = 0; 
 
   
 	this.init = function() {
@@ -44,11 +42,11 @@ var Map = function(mapDiv) {
   };
   
 	Map.prototype.enableZoom = function() {
-    var c = $('#viewport'),  wr = $('#map'), z = $('#zoom');
+    var c = $('#viewport'),  z = $('#zoom'), m = this;
     var viewPortHeight = 480,
         viewPortWidth = 640;
     var ratio = 1;
-    wr.css({
+    m.mapRoot.css({
         'height': viewPortHeight * ratio + 'px',
         'width': viewPortWidth * ratio + 'px'
     });
@@ -57,15 +55,18 @@ var Map = function(mapDiv) {
     //Increase moveSmooth for more moving intensity when zooming
     //When moveSmooth = 1, pointed point is centered at every mousewheel impulsion
     var zoomFactorInvertLog = 1 / Math.log(zoomFactor);
+    
+    
     c.on('mousewheel', function(e, delta) {
         var cOffset = c.offset();
         mouseLocation.x = e.pageX - cOffset.left;
         mouseLocation.y = e.pageY - cOffset.top;
-        var newZoom = Math.max(minZoom, Math.min(maxZoom, currentScale * (1 + delta * zoomFactor)));
-        var sliderVal = Math.log(newZoom) * zoomFactorInvertLog;
+        m.zoom = Math.max(minZoom, Math.min(maxZoom, currentScale * (1 + delta * zoomFactor)));
+        var sliderVal = Math.log(m.zoom) * zoomFactorInvertLog;
         if(slidInvert) sliderVal = slidMin + slidMax - sliderVal;
         z.slider('value', sliderVal);
-        zoom(newZoom);
+        //m.onZoomChange(currentLocation, mouseLocation);
+        zoom();
     });
     var slidMin = Math.log(minZoom) * zoomFactorInvertLog, slidMax = Math.log(maxZoom) * zoomFactorInvertLog;
     var slidInvert = (slidMin > slidMax);
@@ -77,16 +78,18 @@ var Map = function(mapDiv) {
         value: slidMin + slidMax
     }).on('slide slidechange', function (event, ui) {
         var v = slidInvert ? slidMin + slidMax - ui.value : ui.value;
-        var newZoom = Math.pow(zoomFactor, v);
-        zoom(newZoom);
+        m.zoom = Math.pow(zoomFactor, v);
+        //m.onZoomChange(currentLocation, mouseLocation);
+        zoom();
     });
-    function zoom(scale)
+    function zoom()
     {
+      var scale = m.zoom;
       var imgScale = 1/scale;
       if(scale <= 1)
       {
-        currentLocation.x = wr.width() / 2;
-        currentLocation.y = wr.height() / 2;
+        currentLocation.x = m.mapRoot.width() / 2;
+        currentLocation.y = m.mapRoot.height() / 2;
       }
       else
       {
@@ -102,16 +105,39 @@ var Map = function(mapDiv) {
           newCssImage[compat[i]+'transform'] = 'scale('+imgScale+')';
           newCss[compat[i]+'transform-origin'] = currentLocation.x + 'px ' + currentLocation.y + 'px';
       }
-      wr.css(newCss);
-      wr.find('.planets img').css(newCssImage);
+      m.mapRoot.css(newCss);
+      m.mapRoot.find('.planets img').css(newCssImage);
       currentScale = scale;
     }
   };
   
   // Events
 
-	Map.prototype.onZoomChange = function(event, delta) {
-
+	Map.prototype.onZoomChange = function() {
+    var scale = this.zoom;
+    var imgScale = 1/scale;
+    if(scale <= 1)
+    {
+      currentLocation.x = wr.width() / 2;
+      currentLocation.y = wr.height() / 2;
+    }
+    else
+    {
+        currentLocation.x += moveSmooth * (mouseLocation.x - currentLocation.x) / currentScale;
+        currentLocation.y += moveSmooth * (mouseLocation.y - currentLocation.y) / currentScale;
+    }
+    var compat = ['-moz-', '-webkit-', '-o-', ''];
+    var newCss = {};
+    var newCssImage = {};
+    for(var i = compat.length - 1; i; i--)
+    {
+        newCss[compat[i]+'transform'] = 'scale('+scale+')';
+        newCssImage[compat[i]+'transform'] = 'scale('+imgScale+')';
+        newCss[compat[i]+'transform-origin'] = currentLocation.x + 'px ' + currentLocation.y + 'px';
+    }
+    wr.css(newCss);
+    wr.find('.planets img').css(newCssImage);
+    currentScale = scale;
   };    
   
 	Map.prototype.onDragStart = function() {
