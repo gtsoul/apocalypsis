46fb5c3a18882c392774ce20f860086a7b8a628b
Arnaud Demaiziere <arnaud.demaiziere@gmail.com>
2013-06-03 16:23:43 +0200

    wip refonte coord

--- client/entity.planet.js
+++ client/entity.planet.js
@@ -60,11 +60,16 @@ var EntitySun = function(json, parent) {
 var EntityPc = function(json, parent) {
 
   EntityPc.prototype.getHtml = function (left, top) {
-    var $pc = $('<img class="coordPoint nozoom" style="width:80px;height:80px;"/>');
-    $pc.attr('src', this.image);
-    $pc.css('left', Math.round(left)+'px');
-    $pc.css('top', Math.round(top)+'px');
-    return $pc;
+    var $coordPoint = $('<div class="coordPoint"/>');
+    var $pc = $('<img class="pc nozoom" style="width:80px;height:80px;"/>');
+    var $extended = $('<img class="extended nozoom" style="width:150px;height:110px;"/>');
+    $pc.attr('src', this.imagePc);
+    $extended.attr('src', this.imageExtended);
+    $coordPoint.css('left', Math.round(left)+'px');
+    $coordPoint.css('top', Math.round(top)+'px');
+    $coordPoint.append($extended);
+    $coordPoint.append($pc);
+    return $coordPoint;
   };   
   
   EntityPc.prototype = new EntitySpaceElement(json, parent);
@@ -72,7 +77,8 @@ var EntityPc = function(json, parent) {
 	this.init = function() {
     EntitySpaceElement.prototype.__loadJson.apply(this, [json, parent]);
     this.type = 'pc';
-    this.image = 'images/apocalypsis/pc.jpg'
+    this.imagePc = 'images/apocalypsis/pc.jpg';
+    this.imageExtended = 'images/apocalypsis/coords.png';
 		this.init = function() {};
 	};  
 
@@ -88,50 +94,47 @@ var EntityCoords = function(json, parent) {
   EntityCoords.prototype.getHtml = function (left, top) {  
     if(this.left == undefined) { this.left = left};
     if(this.top == undefined) { this.top = top};
-    var $coords = $('<div class="coords unloaded" style="width:50px;height:50px;"/>');
+    var $coords = $('<div class="coords unloaded"/>');
     $coords.attr('id', this.pos);    
     // coordPoint
     var $coordPoint = this.pc.getHtml(this.left + this.widthPx/2, this.top + this.widthPx/2);
     $coordPoint.css('left', Math.round(this.left)+'px');
     $coordPoint.css('top', Math.round(this.top)+'px'); 
-    var $coordPointPc = $coordPoint.clone();    
-    $coordPoint.attr('src', this.image);  
-    $coords.append($coordPoint.addClass('extended').css('width','150').css('height','110'));
+    $coords.append($coordPoint);
     if(this.fleets != undefined) {
       var nbFleets = 0;
       for(var fleetId in this.fleets) {
         nbFleets++;
       }
-      $coordPointPc.attr('fleets', nbFleets); 
+      $coordPoint.attr('fleets', nbFleets); 
     }
-    $coords.append($coordPointPc.addClass('pc'));    
     // planets    
     var $planets = $('<div class="planets"/>');
     if(this.planets != undefined) {
       for(var planetId in this.planets) {
         var planet = this.planets[planetId];
-        var $planet = planet.getHtml(this.left + (planet.x*this.widthPx/this.width), this.top + (planet.y*this.widthPx/this.width))
-        var line = new UiLink($coordPointPc, $planet, /*this.pos+'#'+planet.pos,*/ 'path', 'coord_to_planet');        
+        var $planet = planet.getHtml(this.left + (planet.x*this.widthPx/this.width), this.top + (planet.y*this.widthPx/this.width));
+        
+        var line = new UiLink($coordPoint, $planet, /*this.pos+'#'+planet.pos,*/ 'path', 'coord_to_planet');        
         $planets.append($planet);
       }
     }
     $coords.append($planets);
     // TODO : add fleets    
-    this.__addEvents($coords);
+    this.__addEvents($coords); // todo : coordpoint extended
     return $coords;
   };
   
   EntityCoords.prototype.__addEvents = function(htmlEl) {
-    if(this.known == true) {
-      htmlEl.mouseover(function() {
-        var coord = globalMap.getEntity($(this).attr('id'));
-        if(coord != undefined && coord.planets == undefined && coord.fleets == undefined && coord.known == true) {
-          coord.planets = new Array();
-          coord.fleets = new Array();
-          globalMap.refreshCoord(coord);      
-        }
-      });
-    }
+    htmlEl.mouseover(function() {
+      var coord = globalMap.getEntity($(this).attr('id'));
+      console.log(coord.pos+' => '+coord.known);
+      if(coord != undefined && coord.planets == undefined && coord.fleets == undefined && coord.known == true) {
+        coord.planets = new Array();
+        coord.fleets = new Array();
+        globalMap.refreshCoord(coord);      
+      }
+    });
   };
   
   EntityCoords.prototype.getPlanet = function(planetId) {
