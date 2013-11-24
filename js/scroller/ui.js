117e7c1b98b81675c6b842b4611cc3d3ec834de7
Arnaud Demaiziere <gtsoul@gmail.com>
2013-11-24 16:54:11 +0100

    Test scroller master

--- js/scroller/ui.js
+++ js/scroller/ui.js
@@ -1,6 +1,6 @@
 // Intialize layout
-var container = document.getElementById("container");
-var content = document.getElementById("content");
+var container = document.getElementById("viewport");
+var content = document.getElementById("map");
 var clientWidth = 0;
 var clientHeight = 0;
 
